39bd35cd7f4ad58a08d6bbb39b6fd313c56c73a7
akbar <akbarfardi0101@gmail.com>
2018-06-17 16:18:14 +0430

    initial commit

---
+++ personal/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request,'personal/basic.html',{'content':['If you would like to contact me, please email me.','hskinsley@gmail.com']})
